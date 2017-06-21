e9432c97cf20436df12bc812c4529ed2136c97fb
arscholtes <alex@ascholtes.me>
2017-06-20 23:53:29 -0400

    midnight

--- script.js
+++ script.js
@@ -5,21 +5,23 @@ const userData = require("./public/36d3402e-data.js")
 
 const app = express();
 
-app.use('/styles', express.static(path.join(__dirname, "/public")
+// app.use('/styles', express.static(path.join(__dirname, "/public"))
 app.engine("mustache", mustacheExpress());
 app.set('views','./views')
 app.set('view engine','mustache')
 
-
+app.use('/css', express.static('public'))
 
 
 app.get("/users/", function (req, res) {
   res.render("users", {users: userData.users})
 });
 
-app.get("/users/:id", function(req, res) {
-
-}
+app.get("/user/:id", function(req, res) {
+  let id = req.params.id;
+  let user = userData.users[id];
+  res.render("user", {users: userData.users[id]})
+});
 
 app.listen(3000, function() {
   console.log("Successfully started express application")
